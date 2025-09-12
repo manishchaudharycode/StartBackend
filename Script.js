99b88c043762a00ebbb675616edf1703fd9dd2c7
Manish Chaudhary <manishchaudharyog@gmail.com>
2025-09-12 22:29:39 +0530

    efs

--- Script.js
+++ Script.js
@@ -1,103 +1,102 @@
-
-const fs = require("node:fs");
-
-
-
-   // use of writeFile()
-
-
-fs.writeFile("message.text", "write file ", (err) => {
-  if (err) console.error(err);
-  else console.log("The file has saved");
-});
-
-       // use of appendFile()
-
-fs.appendFile("message.text", " data to append", (err)=> {
-    if(err) console.error(err);
-    else console.log("append")
-
-})
-
-        // use of copyFile()
-
-fs.copyFile("message.text", "copy.text", (err)=> {
-    if(err) console.error(err);
-    else console.log("copyfile")
-
-})
-
-           // use of rename()
-
-fs.rename("message.text", "file.text", (err)=> {
-    if(err) console.error(err);
-    else console.log("rename file ")
-
-})
-
-          // use of inlink
-
-fs.unlink("file.text", (err)=> {
-    if(err) console.error(err)
-    else console.log("unlink file ")
-})
-
-       // use of stat
-
-fs.stat("message.text", (err, stats) => {
-  if(err) console.log(err)
-    else console.log("is file?", stats.isFile())
-})
-
-
-         // use of readFile
- fs.readFile("message.text",(err, data ) => {
-    if(err) console.error(err)
-        else console.log("file data:", data.toString())
- })
-
-
-             // EXPRESS FRAMEWORK
-
-    const { error } = require("console"); 
-const express = require("express");
-const app = express();
-const path = require("path")
-
-
-app.set('view engine', 'ejs');
+// const fs = require("node:fs");
 
 
 
+//    // use of writeFile()
 
-app.get("/", (req, res) => {
-  res.render("Script");
-});
+
+// fs.writeFile("message.text", "write file ", (err) => {
+//   if (err) console.error(err);
+//   else console.log("The file has saved");
+// });
+
+//        // use of appendFile()
+
+// fs.appendFile("message.text", " data to append", (err)=> {
+//     if(err) console.error(err);
+//     else console.log("append")
+
+// })
+
+//         // use of copyFile()
+
+// fs.copyFile("message.text", "copy.text", (err)=> {
+//     if(err) console.error(err);
+//     else console.log("copyfile")
+
+// })
+
+//            // use of rename()
+
+// fs.rename("message.text", "file.text", (err)=> {
+//     if(err) console.error(err);
+//     else console.log("rename file ")
+
+// })
+
+//           // use of inlink
+
+// fs.unlink("file.text", (err)=> {
+//     if(err) console.error(err)
+//     else console.log("unlink file ")
+// })
+
+//        // use of stat
+
+// fs.stat("message.text", (err, stats) => {
+//   if(err) console.log(err)
+//     else console.log("is file?", stats.isFile())
+// })
+
+
+//          // use of readFile
+//  fs.readFile("message.text",(err, data ) => {
+//     if(err) console.error(err)
+//         else console.log("file data:", data.toString())
+//  })
+
+
+//              // EXPRESS FRAMEWORK
+
+//     const { error } = require("console"); 
+// const express = require("express");
+// const app = express();
+// const path = require("path")
+
+
+// app.set('view engine', 'ejs');
+
+
+
+
+// app.get("/", (req, res) => {
+//   res.render("Script");
+// });
  
-app.get("/login", (req, res)=>{
-    res.send("Hello world ");
-})
+// app.get("/login", (req, res)=>{
+//     res.send("Hello world ");
+// })
 
-app.get("/profile/:username", (req, res)=>{
-   res.send(`welocme, ${req.params.username}`)
+// app.get("/profile/:username", (req, res)=>{
+//    res.send(`welocme, ${req.params.username}`)
 
-})
-app.get("/profile/:username/:age", (req, res)=>{
-   res.send(req.params)
+// })
+// app.get("/profile/:username/:age", (req, res)=>{
+//    res.send(req.params)
 
-})
+// })
 
 
-app.use((err, req,res, next)=>{
-    console.error(err.stack)
-    res.status(500).send("something broken")
+// app.use((err, req,res, next)=>{
+//     console.error(err.stack)
+//     res.status(500).send("something broken")
     
-})
+// })
 
 
-app.listen(4000, ()=> {
-    console.log("server is runing on " + 4000)
-});
+// app.listen(4000, ()=> {
+//     console.log("server is runing on " + 4000)
+// });
 
 
 
---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const fs = require("fs");
+
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  // console.log(path);
+
+  fs.readdir("./files", (err, files) => {
+    res.render("index", { files: files });
+  });
+});
+
+app.get("/file/:filename", (req, res)=> {
+    fs.readFile(`./files/${req.params.filename}`, 'utf-8', (err, filedata)=>{
+        res.render("show")
+    })
+})
+
+
+app.post("/create", (req, res)=>{
+   fs.writeFile(`./files/${req.body.Title.split(' ').join('')}.txt`, req.body.details, (err)=> {
+      res.redirect("/")
+   })
+})
+
+app.listen(3000);
